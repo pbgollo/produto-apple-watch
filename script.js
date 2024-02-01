bbcfc980e5d8dbe9580faab4b978abaf99c97aec
Pedro Gollo <130512644+pbgollo@users.noreply.github.com>
2024-02-01 12:51:17 +0000

    Criação dos objetos relacionados as cores

---
+++ script.js
@@ -0,0 +1,26 @@
+const verdeCipreste = {
+    nome: "Verde-cipreste",
+    pasta: "imagens-verde-cipreste"
+};
+
+const azulInverno = {
+    nome: "Azul-inverno",
+    pasta: "imagens-azul-inverno"
+};
+
+const meiaNoite = {
+    nome: "Meia-noite",
+    pasta: "imagens-meia-noite"
+};
+
+const estelar = {
+    nome: "Estelar",
+    pasta: "imagens-estelar"
+};
+
+const rosaClaro = {
+    nome: "Rosa-claro",
+    pasta: "imagens-rosa-claro"
+};
+
+const minhaLista = [6]
